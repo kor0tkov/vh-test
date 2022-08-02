0d1558f17c522d9d0f2d5f5b8e468abfa2854615
Konstantin <quanoxy@gmail.com>
2022-08-02 21:57:19 +0500

    simple gallery

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
+<template>
+  <div class="app">
+    <header class="header">HEADER GOES HERE</header>
+    <main class="gallery">
+      <div v-for="item in 16" :key="item" class="gallery__img" />
+    </main>
+    <footer class="footer">FOOTER GOES HERE</footer>
+  </div>
+</template>
+
 <style>
+html {
+  box-sizing: border-box;
+  font-family: sans-serif;
+  height: -webkit-fill-available;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+}
+body {
+  height: 100%;
+  margin: 0;
+}
+.app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  height: -webkit-fill-available;
+}
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+  background-color: #4f7aff;
+  border-bottom: 1px solid black;
+}
+.footer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+  background-color: #4f7aff;
+  border-top: 1px solid black;
+}
+.gallery {
+  overflow-y: auto;
+  display: flex;
+  flex-wrap: wrap;
+  flex: 1;
+  padding: 2px;
+  background-color: black;
+}
+.gallery__img {
+  width: calc(100% / 3 - 4px);
+  height: calc(100vw / 3 - 4px);
+  margin: 2px;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-image: url('https://i.gaw.to/vehicles/photos/08/28/082870_2017_ferrari_488.jpg');
 }
 </style>
